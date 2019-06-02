5414d44430580d2c52eceec6e120f5e0da5be3f3
Marina Papilina <marina230495@mail.ru>
2019-06-02 14:32:41 +0300

    добавила страницу Помощь, поля создания места

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -30,6 +30,10 @@ namespace MVC.Controllers
             return View();
         }
 
+        public IActionResult Help()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
